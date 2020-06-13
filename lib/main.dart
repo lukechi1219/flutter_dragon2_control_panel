2bf41307eeab1977f08609c663b22948283f358d
Luke Chi <luke.chi@gmail.com>
2020-06-13 20:10:31 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 
 void main() {
   runApp(MyApp());
@@ -48,11 +49,10 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
 
-    _iframeElement.height = '500';
-    _iframeElement.width = '500';
-
-    _iframeElement.src = 'https://www.youtube.com/embed/EEIk7gwjgIM';
+    _iframeElement.src = 'https://www.youtube.com/embed/DDU-rZs-Ic4?autoplay=1';
     _iframeElement.style.border = 'none';
+    _iframeElement.allow =
+        'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
 
     // ignore: undefined_prefixed_name
     ui.platformViewRegistry.registerViewFactory(
@@ -69,58 +69,174 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+//      appBar: AppBar(
+//        title: Text(widget.title),
+//      ),
       body: Center(
         child: Column(
           children: [
             Expanded(
               child: Row(
                 children: [
-                  Expanded(
-                    child: Container(
-                      color: Colors.red,
-                      child: Text('red'),
-                    ),
+                  uiBlock(
+                    title: 'Crew',
+                    value: 'DRAGON',
                   ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.amber,
-                      child: Text('blue'),
-                    ),
+                  uiBlock(
+                    title: 'Perigee',
+                    value: '185.013m',
                   ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.green,
-                      child: Text('blue'),
-                    ),
+                  uiBlock(
+                    title: 'Apogee',
+                    value: '360.304m',
                   ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.blue,
-                      child: Text('blue'),
-                    ),
+                  uiBlock(
+                    title: 'Relative Altitude',
+                    value: '50,948m',
                   ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.orange,
-                      child: Text('blue'),
-                    ),
+                  uiBlock(
+                    title: 'Range to ISS',
+                    value: '625,713m',
                   ),
                 ],
               ),
             ),
             Expanded(
+              flex: 4,
               child: Container(
                 color: Colors.blueGrey,
                 child: _iframeWidget,
               ),
             ),
             Expanded(
-              child: Container(
-                color: Colors.grey,
-                child: Text('grey'),
+              child: Row(
+                children: [
+                  uiBlock(
+                    title: 'Day 131',
+                    value: '13:46:11',
+                  ),
+                  uiBlock(
+                    value: '625,713m',
+                  ),
+                  uiBlock(
+                    title: 'Range to ISS',
+                    value: '625,713m',
+                  ),
+                  uiBlock(
+                    title: 'Range to ISS',
+                    value: '625,713m',
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  uiBlock(
+                    value: '0 ALARMS, ALL SYSTEMS NORMAL',
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  uiBlock(
+                    title: 'Range to ISS',
+                    value: '625,713m',
+                  ),
+                  uiBlock(
+                    title: 'Range to ISS',
+                    value: '625,713m',
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  uiBlock(
+                    title: 'Range to ISS',
+                    value: '625,713m',
+                  ),
+                  uiBlock(
+                    title: 'Range to ISS',
+                    value: '625,713m',
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: [
+                  uiBlock(
+                    title: 'Range to ISS',
+                    value: '625,713m',
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: [
+                  uiBlock(
+                    title: 'Range to ISS',
+                    value: '625,713m',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Expanded uiBlock({
+    String title,
+    @required String value,
+  }) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: Column(
+          children: [
+            (title != null)
+                ? widthExpanded(
+                    Colors.grey,
+                    Text(
+                      title,
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  )
+                : Container(),
+            widthExpanded(
+              Colors.blueGrey,
+              Text(
+                value,
+                style: TextStyle(fontSize: 24),
+              ),
+              2,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Expanded widthExpanded(Color _color, Text _child, [int _flex]) {
+    return Expanded(
+      flex: _flex,
+      child: Container(
+        color: _color,
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: _child,
               ),
             ),
           ],
